138e1e62854df711c2b0b681ed89645e6647c4c3
Ryan Hollins <ballbaroi@gmail.com>
2020-02-27 17:57:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Add your code here
+require "pry"
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    binding.pry
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
